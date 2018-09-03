9fa3f3b2930b0d17e39c7df728ad15332ce29164
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-09-04 08:02:40 +0900

    support submissions from old AtCoder site

--- index.user.js
+++ index.user.js
@@ -6,8 +6,22 @@
 // @author       Kimiyuki Onaka
 // @match        *://beta.atcoder.jp/contests/*/submit
 // @match        *://beta.atcoder.jp/contests/*/tasks/*
+// @match        *://*.contest.atcoder.jp/submit*
 // ==/UserScript==
-function main() {
+function post(path, payload, expectedURL) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", path, false);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(payload);
+    console.log(xhr);
+    if (xhr.responseURL == expectedURL) {
+        location.href = xhr.responseURL;
+    }
+    else {
+        alert("AtCoder dos2unix UserScript: something wrong");
+    }
+}
+function beta() {
     const taskScreenName = document.getElementsByName("data.TaskScreenName")[0];
     const languageId = document.getElementsByName("data.LanguageId")[0];
     const sourceCode = document.getElementsByName("sourceCode")[0];
@@ -16,7 +30,6 @@ function main() {
     submit.addEventListener("click", function (e) {
         e.preventDefault();
         const contestId = location.pathname.split('/')[2];
-        const path = "/contests/" + contestId + "/submit";
         const data = [];
         for (const tag of [taskScreenName, languageId, sourceCode, csrfToken]) {
             var value = tag.value;
@@ -28,17 +41,38 @@ function main() {
         }
         const payload = data.join('&');
         console.log(payload);
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", path, false);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.send(payload);
-        console.log(xhr);
-        if (xhr.responseURL == "https://beta.atcoder.jp/contests/" + contestId + "/submissions/me") {
-            location.href = xhr.responseURL;
-        }
-        else {
-            alert("AtCoder dos2unix UserScript: something wrong");
-        }
+        const path = "/contests/" + contestId + "/submit";
+        const expectedURL = location.origin + "/contests/" + contestId + "/submissions/me";
+        post(path, payload, expectedURL);
     });
+}
+function alpha() {
+    const session = document.getElementsByName("__session")[0];
+    const taskId = document.getElementsByName("task_id")[0];
+    const languageId207 = document.getElementsByName("language_id_207")[0];
+    const languageId2520 = document.getElementsByName("language_id_2520")[0];
+    const sourceCode = document.getElementsByName("source_code")[0];
+    const submit = document.getElementsByTagName("button")[0];
+    submit.addEventListener("click", function (e) {
+        e.preventDefault();
+        const data = [];
+        for (const tag of [session, taskId, languageId207, languageId2520, sourceCode]) {
+            data.push(tag.name + '=' + encodeURIComponent(tag.value));
+            console.log([tag.name, tag.value]);
+        }
+        const payload = data.join('&');
+        console.log(payload);
+        const path = "/submit?task_id=" + taskId.value;
+        const expectedURL = location.origin + "/submissions/me";
+        post(path, payload, expectedURL);
+    });
+}
+function main() {
+    if (location.hostname == "beta.atcoder.jp") {
+        beta();
+    }
+    else {
+        alpha();
+    }
 }
 main();
